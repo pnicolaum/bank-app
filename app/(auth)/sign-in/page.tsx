2084953e3d2a60a18740b1f77e7c0910980f0616
pnicolaum <p@nicolau.com>
2025-07-24 16:21:35 +0200

    left sidebar ui and hero

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function SignIn() {
   return (
     <div>
-      SignIn page
+      SignIn
     </div>
   )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 function Home() {
+  const loggedIn = {firstName: "Pablo"}
+
   return (
-    <div>
-      Home page
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            subtext="Access and manage your account and transactions efficiently"
+            user={loggedIn.firstName || "Guest"}
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
